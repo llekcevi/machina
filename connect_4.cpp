779983a0967b1b4a4d4f0c00b035ef0517b6c488
Luka Lekcevic <luka.lekcevi@gmail.com>
2022-11-03 20:53:54 +0100

    Added function to show  the board

--- connect_4.cpp
+++ connect_4.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <algorithm>
 
+void showBoard(char game_board[6][7]);
+
 int main() {
     bool first_players_turn = true;
     bool win = false;
@@ -9,13 +11,10 @@ int main() {
     //intializes array "board" to '_'
     for (int i = 0; i < 7; i++) {memset(board[i],'_', 7);}
     //
-    //prints out the board
-    std::cout<<"1 2 3 4 5 6 7"<<std::endl;
-    for (int i = 5; i >= 0; i--) {
-        std::cout<<board[i][0]<<" "<<board[i][1]<<" "<<board[i][2]<<" "<<board[i][3]<<" "<<board[i][4]<<" "<<board[i][5]<<" "<<board[i][6]<<std::endl;
-    }
-    //
     while (win == false) {
+        //prints out the board
+        showBoard(board);
+        //
         //player chooses a column to play
         first_players_turn ? std::cout<<"X choose a column: "<<std::endl : std::cout<<"O chose a column"<<std::endl;
         std::cin>>chosen_column;
@@ -27,15 +26,17 @@ int main() {
             } ;
         } 
         //
-        //prints out the board
-        std::cout<<"1 2 3 4 5 6 7"<<std::endl;
-        for (int i = 5; i >= 0; i--) {
-        std::cout<<board[i][0]<<" "<<board[i][1]<<" "<<board[i][2]<<" "<<board[i][3]<<" "<<board[i][4]<<" "<<board[i][5]<<" "<<board[i][6]<<std::endl;
-        }
         first_players_turn = !first_players_turn;
         //
     }
 }
 
-        
+//F U N C T I O N S    
+void showBoard (char game_board[6][7]){    
+    std::cout<<"1 2 3 4 5 6 7"<<std::endl;
+    for (int i = 5; i >= 0; i--) {
+        std::cout<<game_board[i][0]<<" "<<game_board[i][1]<<" "<<game_board[i][2]<<" "<<game_board[i][3]<<" "<<game_board[i][4]<<" "<<game_board[i][5]<<" "<<game_board[i][6]<<std::endl;
+    }
+}    
+//
 
