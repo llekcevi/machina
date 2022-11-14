913ece717425367afc0cbdcab2ecf459292df7db
Luka Lekcevic <luka.lekcevi@gmail.com>
2022-11-14 15:39:45 +0100

    Added functions and changed names

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -1,49 +1,17 @@
 #include <iostream>
 #include <algorithm>
 
-void show_board(char game_board[]);
-bool check_winner(char game_board[]);
-void game_over(char game_board[], bool turn);
-
-int main()
+void InitializeBoard(char gameBoard[]) 
 {
-    bool win = false;
-    bool first_players_turn = true;
-    int position;
+    std::fill_n(gameBoard, 9, '_');
 
-    char board[9];
-    std::fill_n(board, 9, '_');
-
-    while (win == false)
-    {
-        show_board(board);
-
-        // player choses where to put a mark
-        std::cout << "Choose your position" << std::endl;
-        std::cin >> position;
-        //
-        // inputs x or o based on player order and position availability then reverses player order
-        if (board[position - 1] == '_')
-        {
-            first_players_turn ? board[position - 1] = 'X' : board[position - 1] = 'O';
-            first_players_turn = !first_players_turn;
-        }
-        else
-        {
-            std::cout << "position taken, chose another one" << std::endl;
-        }
-        //
-        win = check_winner(board);
-    }
-    game_over(board, first_players_turn);
-
-    return 0;
 }
-void show_board(char game_board[9])
+
+void ShowBoard(char gameBoard[9])
 {
     for (int i = 0; i < 9; i++)
     {
-        std::cout << game_board[i]<<' ';
+        std::cout << gameBoard[i]<<' ';
         if (i % 3 == 2)
         {
             std::cout << std::endl;
@@ -51,16 +19,16 @@ void show_board(char game_board[9])
     }
 }
 
-bool check_winner(char game_board[])
+bool CheckWinner(char gameBoard[])
 {
-    bool first_row = ((game_board[0] == game_board[1]) && (game_board[0] == game_board[2])) && (game_board[0] != '_');
-    bool second_row = ((game_board[3] == game_board[4]) && (game_board[3] == game_board[5])) && (game_board[3] != '_');
-    bool third_row = ((game_board[6] == game_board[7]) && (game_board[6] == game_board[8])) && (game_board[6] != '_');
-    bool first_column = ((game_board[0] == game_board[3]) && (game_board[0] == game_board[6])) && (game_board[0] != '_');
-    bool second_column = ((game_board[2] == game_board[4]) && (game_board[2] == game_board[7])) && (game_board[2] != '_');
-    bool third_column = ((game_board[3] == game_board[5]) && (game_board[3] == game_board[8])) && (game_board[3] != '_');
-    bool left_to_right_d = ((game_board[0] == game_board[4]) && (game_board[0] == game_board[8])) && (game_board[0] != '_');
-    bool right_to_left_d = ((game_board[3] == game_board[4]) && (game_board[3] == game_board[6])) && (game_board[3] != '_');
+    bool first_row = ((gameBoard[0] == gameBoard[1]) && (gameBoard[0] == gameBoard[2])) && (gameBoard[0] != '_');
+    bool second_row = ((gameBoard[3] == gameBoard[4]) && (gameBoard[3] == gameBoard[5])) && (gameBoard[3] != '_');
+    bool third_row = ((gameBoard[6] == gameBoard[7]) && (gameBoard[6] == gameBoard[8])) && (gameBoard[6] != '_');
+    bool first_column = ((gameBoard[0] == gameBoard[3]) && (gameBoard[0] == gameBoard[6])) && (gameBoard[0] != '_');
+    bool second_column = ((gameBoard[2] == gameBoard[4]) && (gameBoard[2] == gameBoard[7])) && (gameBoard[2] != '_');
+    bool third_column = ((gameBoard[3] == gameBoard[5]) && (gameBoard[3] == gameBoard[8])) && (gameBoard[3] != '_');
+    bool left_to_right_d = ((gameBoard[0] == gameBoard[4]) && (gameBoard[0] == gameBoard[8])) && (gameBoard[0] != '_');
+    bool right_to_left_d = ((gameBoard[3] == gameBoard[4]) && (gameBoard[3] == gameBoard[6])) && (gameBoard[3] != '_');
 
     if (
         ((first_row || second_row) || (third_row || first_column)) ||
@@ -73,11 +41,51 @@ bool check_winner(char game_board[])
     return false;
 };
 
-void game_over(char game_board[], bool turn)
+void GameOver(char gameBoard[], bool turn)
 {
     std::cout << std::endl;
-    show_board(game_board);
+    ShowBoard(gameBoard);
     std::cout << std::endl;
     std::cout << "game over" << std::endl;
     std::cout << (turn ? "O wins" : "x wins") << std::endl;
+}
+
+void InputMove(char board[], bool firstPlayersTurn) 
+{   
+    
+    int position;// player choses where to put a mark
+    std::cout << "Choose your position" << std::endl;
+    std::cin >> position;
+    //
+    // inputs x or o based on player order and position availability then reverses player order
+    if (board[position - 1] == '_')
+    {
+        firstPlayersTurn ? board[position - 1] = 'X' : board[position - 1] = 'O';
+    }
+    else
+    {
+        std::cout << "position taken, chose another one" << std::endl;
+    }
+}
+
+
+int main()
+{
+    bool win = false;
+    bool first_players_turn = true;
+    int position;
+    char board[9];
+
+    InitializeBoard(board);
+
+    while (!win)
+    {
+        ShowBoard(board);
+        InputMove(board, first_players_turn);
+        first_players_turn = !first_players_turn;
+        win = CheckWinner(board);
+    }
+    GameOver(board, first_players_turn);
+
+    return 0;
 }
