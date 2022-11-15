8a3f2cbfbbcd0015a027b3c50be62037a3314d8f
Luka Lekcevic <luka.lekcevi@gmail.com>
2022-11-15 13:13:10 +0100

    Updated .gitingnore

---
+++ fifth_lecture.cpp
@@ -0,0 +1,17 @@
+/* 
+polja i funkcije
+polja - npr. nizovi
+int brojevi[8] - deklaracija varijable tipa niz koji u sebi sadrži 8 elemenata
+kad tu rezerviraš taj array i pozoveš npr brojevi[10], to mjesto u ramu postoji i dostupno je kompajleru, ali može uzrokovati bugove
+
+#define N 9 poslije iostreama
+to je preprocessorska naredba - postavke prije kompajliranja
+
+for petlja za ispisivanje tictactoe arraya koja ubacuje novi red
+za svaki član koji daje ostatk 2 pri dijeljenju s 3
+
+rand()
+
+uvijek je bolje funkcijama proslijediti podatke, nego ih imati u globalnom scopeu
+
+ */
