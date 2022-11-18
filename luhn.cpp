393e86be0cd23c1c81ea3043a32750b26c06cfd3
Luka Lekcevic <luka.lekcevi@gmail.com>
2022-11-18 18:26:51 +0100

    Program now recognizes check digit 0 and takes user input

--- luhn.cpp
+++ luhn.cpp
@@ -1,36 +1,47 @@
 #include <iostream>
 #include <string>
-//5437 7805 4231 787 ?
+// 5437 7805 4231 787 ?
 
-int main() {
-    std::cout << "Luhn Algorithm" << std::endl;
-
-    std::string number = "543778054231787";
-    std::string digits_ready;
-    int sum;
+int CalucalteCheckDigit(std::string creditCardNumber)
+{
+    std::string ready_for_sum;
+    int sum=0;
     int last_digit;
-
-    for (int i = 0; i < number.size(); i++) {
-        if (i%2 == 0) {
-            char digit = number[i];
-            int idigit = (digit - '0')*2;
-            if (idigit > 9) {idigit = 1 + idigit%10;}
-            //std::cout<<idigit<<std::endl;
-            digits_ready += std::to_string(idigit);
+    for (int i = 0; i < creditCardNumber.size(); i++)
+    {
+        if (i % 2 == 0)
+        {
+            char digit = creditCardNumber[i];
+            int intDigit = (digit - '0') * 2;
+            if (intDigit > 9)
+            {
+                intDigit = 1 + intDigit % 10;
+            }
+            ready_for_sum += std::to_string(intDigit);
         }
-        else {digits_ready += number[i];}
-        //std::cout << number[i] << std::endl;;
+        else
+        {
+            ready_for_sum += creditCardNumber[i];
+        }
     }
 
-    for (int i = 0; i < digits_ready.size(); i++) {
-        int digit = (digits_ready[i])-'0';
+    for (int i = 0; i < ready_for_sum.size(); i++)
+    {
+        int digit = (ready_for_sum[i]) - '0';
         sum += digit;
     }
+    last_digit = (10 - sum % 10) % 10;
+    return last_digit;
+}
 
-    last_digit = 10 - sum % 10;
-    std::cout << number << std::endl;
-    /* std::cout << digits_ready << std::endl;
-    std::cout<<sum<<std::endl; */
-    std::cout<<"Last digit is "<<last_digit<<std::endl;
+int main()
+{
+    std::cout << "Luhn's Algorithm" << std::endl;
+
+    std::string number;
+    std::cout << "enter credit card number: " << std::endl;
+    std::cin>>number;
+    std::cout<<"Check digit is: "<<CalucalteCheckDigit(number)<<std::endl;
+    
     return 0;
 }
