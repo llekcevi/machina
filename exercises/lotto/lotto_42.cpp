b80ae41286b3f130a9069d1ca545154b21d09a7a
Luka Lekcevic <luka.lekcevi@gmail.com>
2022-12-03 13:23:30 +0100

    Added header and separate functions file

--- exercises/lotto/lotto_42.cpp
+++ exercises/lotto/lotto_42.cpp
@@ -1,68 +1,6 @@
 #include <iostream>
-
-bool CheckForDuplicates(int array[], int number)
-{
-    for (int i = 0; i < 7; i++)
-    {
-        if (number == array[i])
-        {
-            return true;
-        }
-        else
-            continue;
-    }
-    return false;
-}
-
-void ChooseNumbers(int chosenNumbers[7])
-{
-    for (int i = 0; i < 7; i++)
-    {
-        bool valid_number = true;
-        int number;
-
-        do
-        {
-            std::cout << "Input " << i + 1 << ". number (1 - 42)" << std::endl;
-            std::cin >> number;
-            valid_number = !CheckForDuplicates(chosenNumbers, number);
-
-        } while (!valid_number);
-        chosenNumbers[i] = number;
-    }
-    std::cout << "-----------------------" << std::endl;
-}
-
-void GenerateRandomNumbers(int randomNumbers[7])
-{
-    for (int i = 0; i < 7; i++)
-    {
-        bool valid_number = true;
-        int number;
-
-        do
-        {   
-            number = rand()%42+1;
-            valid_number = !CheckForDuplicates(randomNumbers, number);
-
-        } while (!valid_number);
-        
-        randomNumbers[i] = number;
-    }
-}
-
-void CheckWin(int randomNumbers[7], int chosenNumbers[7], int winNumberCounter)
-{
-    for (int r = 0; r < 7; r++)
-    {
-        for (int c = 0; c < 7; c++)
-        {
-            if (randomNumbers[r] == chosenNumbers[c])
-                std::cout << "you have guessed: " << chosenNumbers[c] << std::endl;
-            winNumberCounter = winNumberCounter + 1;
-        }
-    }
-}
+#include "lotto_42.h"
+#include "lotto_42_functions.cpp"
 
 int main()
 {
@@ -73,13 +11,7 @@ int main()
 
     GenerateRandomNumbers(random_numbers);
     ChooseNumbers(chosen_numbers);
-
-    for (int i = 0; i < 7; i++)
-    {
-        std::cout << random_numbers[i] << std::endl;
-    }
-    std::cout << "-----------------------" << std::endl;
-
+    ShowRandomNumbers(random_numbers);
     CheckWin(random_numbers, chosen_numbers, win_number_counter);
     return 0;
 }
---
+++ exercises/lotto/lotto_42.h
@@ -0,0 +1,9 @@
+bool CheckForDuplicates(int array[], int number);
+
+void ChooseNumbers(int chosenNumbers[7]);
+
+void GenerateRandomNumbers(int randomNumbers[7]);
+
+void ShowRandomNumbers(int randomNumbers[7]);
+
+void CheckWin(int randomNumbers[7], int chosenNumbers[7], int winNumberCounter);
