e0200b98d3ead57da221958f0c1a2a81d85b7ab7
Luka Lekcevic <luka.lekcevi@gmail.com>
2022-12-03 15:07:58 +0100

    Lecture notes added

---
+++ notes/eight_lecture.cpp
@@ -0,0 +1,13 @@
+/* 
+objekti
+
+
+(vraćanje nizova iz funkcija ćemo kasnije odraditi)
+
+static varijabla? - "izvjegavati dok ne dođe dan kad trebaš"
+    - unutar funkcije pamti svoju vrijednost nakon svakog poziva funkcije, za razliku od obične koja se resetira na svakom pozivu
+    - singleton pattern? - za to je dobro
+
+extern int j; 
+    - u hedaeru znači da negdje postoji ta varijabla i ne treba je inicijalizirati
+ */
