bbfd8654b2e35b809d35a54dc1c7ec2b72bf235c
Luka Lekcevic <luka.lekcevi@gmail.com>
2022-12-17 23:13:57 +0100

    Sfml exercise and lecture notes

---
+++ notes/10.cpp
@@ -0,0 +1,16 @@
+/* 
+sfml
+
+upis vanjskog libraryja u projekt
+kod>compile>linker>exe
+
+.dll dynamicaly loaded library - program je čita dinamički
+- nemaju main funkciju, nisu executable fileovi
+sfml može i statički i dinamički
+
+include < >
+
+kreni od window, system, graphics
+
+model, view, controller 
+ */
