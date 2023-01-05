a5a7569299b73373acad307f95bc3d165fcf8e5b
Luka Lekcevic <luka.lekcevi@gmail.com>
2023-01-05 20:10:37 +0100

    Added rabbit sprite

--- exercises/sfml/sfml_exercise.cpp
+++ exercises/sfml/sfml_exercise.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
 #include <SFML/System.hpp>
 #include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
     // Declare and create a new window
-    sf::Window window(sf::VideoMode(800, 600), "SFML window");
+    sf::RenderWindow window(sf::VideoMode(1366, 768), "SFML window");
     // Limit the framerate to 60 frames per second (this step is optional)
     window.setFramerateLimit(60);
+    
+    //SETUP
+    sf::Texture zec_tekstura;
+    zec_tekstura.loadFromFile("/Users/llekcevic/Downloads/kenney-animalpack/PNG/Round/rabbit.png");
+
+    sf::Sprite zec_sprite;
+    zec_sprite.setTexture(zec_tekstura);
+    // GAME ENGINE LOOP
+
+
     // The main loop - ends as soon as the window is closed
     while (window.isOpen())
     {
@@ -21,6 +32,10 @@ int main()
         }
         // Activate the window for OpenGL rendering
         window.setActive();
+        //DRAW
+
+        window.draw(zec_sprite);
+
         // OpenGL drawing commands go here...
         // End the current frame and display its contents on screen
         window.display();
